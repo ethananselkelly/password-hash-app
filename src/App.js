dda7dbbe9f24877222b08870653312691fe7f682
ethananselkelly <eanselke@gmail.com>
2022-04-06 18:11:58 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import axios from 'axios'
 
-function App() {
+const App = () => {
+  const [username, setUsername] = useState(null)
+  const [password, setPassword] = useState(null)
+  const [confirmPassword, setConfirmPassword] = useState(null)
+  const [error, setError] = useState(null)
+
+  
+  const handleSubmit = (event) => {
+    event.preventDefault()
+    if (password !== confirmPassword) {
+      setError('**make sure passwords match!**')
+      return
+    }
+    
+    axios.post('/signup', {
+      username,
+      password
+    })
+
+    console.log('username',username)
+    console.log('password',password)
+    console.log('confirmPassword',confirmPassword)
+    setError(null)
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <form onSubmit={handleSubmit}>
+      <input
+        type='text'
+        id='username'
+        name='username'
+        placeholder='username'
+        required
+        onChange={(event) => setUsername(event.target.value)}
+      />
+      <input
+        type='text'
+        id='password'
+        name='password'
+        placeholder='password'
+        required
+        onChange={(event) => setPassword(event.target.value)}
+      />
+      <input
+        type='text'
+        id='password-check'
+        name='password-check'
+        placeholder='confirm password'
+        required
+        onChange={(event) => setConfirmPassword(event.target.value)}
+      />
+      <input type='submit'/>
+      <p>{error}</p>
+    </form>
   );
 }
 
